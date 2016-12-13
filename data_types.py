f74d4e4dd697aa8613436533e17d95e8c6e96fd4
Joel M. Allen <joel.m.allen@comcast.net>
2016-12-13 09:55:17 -0800

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -1,4 +1,8 @@
 print("Hello World!")
+
+# script to play with data types: examine the implicit type changes with some math ops, and the direct
+#   typing functioons int() and float()
+# created on python 2.7.12 on windows, with IDLE.
        
 a = raw_input('Enter a Number: ')
 b = raw_input('Now Enter another Number: ')
