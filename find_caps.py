911c90837340442fceb60c1957d0a69ed3a46ac4
Joel M. Allen <joel.m.allen@comcast.net>
2016-12-15 14:47:33 -0800

    Update find_caps.py
    add problem statement

--- find_caps.py
+++ find_caps.py
@@ -1,4 +1,33 @@
+#----------------------------------------------
+#find_caps() -problem statement
+#----------------------------------------------
 
+#You are given a chunk of text. 
+#Gather all capital letters in one word in the order that they appear in the text.
+
+#Input: A text as a string.
+
+#Output: Capital letters in the string.
+
+#Example:
+
+#	find_caps("George Washington") == "GW"
+#	find_caps("John Adams") == "JA"
+#	find_caps("wHat AbouT This Example of Reasonable text?") == "HATTER"
+#	find_caps("") == ""
+    
+#Precondition: 
+#- Input text will be a string with length between 0 and 1000.
+#- All characters in input text are printable.
+
+#Your Task:
+
+#1. Replace body of find_caps() with something that works.
+
+#def find_caps(text):
+#    return ""
+
+#2. Write some test code to prove your solution works.
 #import module for unit testing:
 import unittest
 
